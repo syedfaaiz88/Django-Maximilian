1caf1f94ff2e2e2035d9cb5d5f6355ca65d1fc18
syedfaizan-dev <faizan.hussain@bigentities.com>
2024-07-21 21:24:16 +0500

    Add Some Admin Panel Configurations

--- blog/admin.py
+++ blog/admin.py
@@ -2,6 +2,12 @@ from django.contrib import admin
 from .models import Post, Tag, Author
 # Register your models here.
 
-admin.site.register(Post)
+
+class PostAdmin(admin.ModelAdmin):
+    list_filter = ("author", "tags", "date",)
+    list_display = ("title", "author", "date",)
+    prepopulated_fields = {"slug": ("title",)}
+
+admin.site.register(Post, PostAdmin)
 admin.site.register(Tag)
 admin.site.register(Author)
--- blog/models.py
+++ blog/models.py
@@ -6,11 +6,17 @@ from django.core.validators import MinLengthValidator
 class Tag(models.Model):
     caption = models.CharField(max_length=20)
 
+    def __str__(self):
+        return f"{self.caption}"
+
 
 class Author(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     email = models.EmailField()
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
 
 
 class Post(models.Model):
