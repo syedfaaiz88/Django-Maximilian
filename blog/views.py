d2a317a84bd737bbb536f860946eff495abb4465
syedfaizan-dev <faizan.hussain@bigentities.com>
2024-07-21 23:34:40 +0500

    Display Tags on Post page

--- blog/views.py
+++ blog/views.py
@@ -22,5 +22,6 @@ def posts(request):
 def post_detail(request, slug):
     post = get_object_or_404(Post, slug=slug)
     return render(request, "blog/post-detail.html", {
-        "post": post
+        "post": post,
+        "tags": post.tags.all()
     })
