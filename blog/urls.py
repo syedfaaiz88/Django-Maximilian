8bcbf5dd305aec70f47c23f6f7cc05d7fbc44b63
syedfaizan-dev <faizan.hussain@bigentities.com>
2024-07-22 22:24:32 +0500

    Add Read Later Feature via Sessions

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path("", views.StartingPagePosts.as_view(), name="index-page"),
     path("posts", views.PostsView.as_view(), name="posts-page"),
-    path("posts/<slug:slug>", views.PostDetailView.as_view(), name="post-detail-page")
+    path("posts/<slug:slug>", views.PostDetailView.as_view(), name="post-detail-page"),
+    path("read-later", views.ReadLaterView.as_view(), name="read-later-page")
 ]
