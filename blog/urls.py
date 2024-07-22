23467b72ac4c3f9ec8955b984d19856a8a7932f6
syedfaizan-dev <faizan.hussain@bigentities.com>
2024-07-22 19:48:23 +0500

    Convert View to Class Based Views

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index-page"),
-    path("posts", views.posts, name="posts-page"),
-    path("posts/<slug:slug>", views.post_detail, name="post-detail-page")
+    path("", views.StartingPagePosts.as_view(), name="index-page"),
+    path("posts", views.PostsView.as_view(), name="posts-page"),
+    path("posts/<slug:slug>", views.PostDetailView.as_view(), name="post-detail-page")
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,27 +1,36 @@
+from typing import Any
+from django.db.models.query import QuerySet
 from django.shortcuts import render, get_object_or_404
 from .models import Post
-
+from django.views.generic import ListView, DetailView
 
 # Create your views here.
 
 
-def index(request):
-    latest_posts = Post.objects.all().order_by("-date")[:3]
-    return render(request, "blog/index.html", {
-        "posts": latest_posts
-    })
+class StartingPagePosts(ListView):
+    model = Post
+    template_name = "blog/index.html"
+    context_object_name = "posts"
+    ordering = ["-date"]
+
+    def get_queryset(self):
+        querySet = super().get_queryset()
+        data = querySet[:3]
+        return data
 
 
-def posts(request):
-    all_posts = Post.objects.all().order_by("-date")
-    return render(request, "blog/posts.html", {
-        "all_posts": all_posts
-    })
+class PostsView(ListView):
+    model = Post
+    template_name = "blog/posts.html"
+    context_object_name = "all_posts"
+    ordering = ["-date"]
 
 
-def post_detail(request, slug):
-    post = get_object_or_404(Post, slug=slug)
-    return render(request, "blog/post-detail.html", {
-        "post": post,
-        "tags": post.tags.all()
-    })
+class PostDetailView(DetailView):
+    model = Post
+    template_name = "blog/post-detail.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["tags"] = self.object.tags.all()
+        return context
